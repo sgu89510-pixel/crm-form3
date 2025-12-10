fc992394809fa71cff6229d031f43751d05a079d
sgu89510-pixel <sgu89510@gmail.com>
2025-12-10 16:27:29 +0400

    server.py

--- server.py
+++ server.py
@@ -19,14 +19,14 @@ def submit():
     # ДАННЫЕ, КОТОРЫЕ УХОДЯТ В CRM
     fields = {
         "api_key": "3f50a5cd6aba6f7cf9be37684d359190",
-        "map_id": 4184,
+        "map_id": 4175,
         "email": request.form.get("email"),
         "first_name": request.form.get("first_name"),
         "second_name": request.form.get("second_name"),
         "phone": request.form.get("phone"),
         "country": "RU",
         "language": "ru",
-        "campaign": "RuAutoEU",
+        "campaign": "RuAutoUS",
         "description": "Лид с лендинга",
         "password": generate_password()
     }
